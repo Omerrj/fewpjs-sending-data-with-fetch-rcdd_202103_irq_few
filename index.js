a96b0ce492a9af5ba557d1125dbc792d887eedb1
Omer Sabah <omerrj99@gmail.com>
2021-05-21 23:26:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your code here
+fetch("http://localhost:3000/users",{
+  method:"POST",
+  body:{
+    name:"Steve",
+    email:"steve@steve.com"
+  },
+  headers:{
+    'content-type':"application/json",
+    'accept':"application/json"
+  }
+}).then(r=>r.json()).then(d=>console.log(d));
