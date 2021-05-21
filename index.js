d51468c8cf0dabc54ef5f7101c54ed3781e93f6c
Omer Sabah <omerrj99@gmail.com>
2021-05-21 23:27:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 // Add your code here
-fetch("http://localhost:3000/users",{
-  method:"POST",
-  body:{
-    name:"Steve",
-    email:"steve@steve.com"
-  },
-  headers:{
-    'content-type':"application/json",
-    'accept':"application/json"
-  }
-}).then(r=>r.json()).then(d=>console.log(d));
+const submitData=( name, email )=> {
+  return fetch( 'http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name,
+        email
+      } )
+    } )
+    .then( function ( response ) {
+      return response.json()
+    } )
+    .then( function ( object ) {
+      document.body.innerHTML = object[ "id" ]
+    } )
+    .catch( function ( error ) {
+      document.body.innerHTML = error.message
+    } )
+}
